54f12325a2de4da07ef74a157c4a286b70893a99
T-Streets <tyler@featherandash.io>
2019-06-01 00:02:53 -0700

    adds route for begin.html + flag

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ app = Flask(__name__, static_folder='static', static_url_path='')
 def home():
 	return render_template('home.html')
 
+@app.route('/begin')
+def begin():
+	return render_template('begin.html')
+
 @app.route('/login')
 def login():
 	return render_template('login.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
