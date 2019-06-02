3013372dc3a3c25e16b743d2606bcd901bc80e95
T-Streets <tyler@featherandash.io>
2019-06-02 16:42:49 -0700

    fixes live counter

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 import re
+from multiprocessing import Value
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.sqlite3'
@@ -43,20 +44,22 @@ def login():
 		return render_template('tables.html', User=User.query.filter_by(password=password, username=username))
 	return render_template('login.html')
 
+lives = Value('i', 3)
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
-	lives = 3
 	if request.method == 'POST':
 		form_data = request.form['form_submit']
 		find_false = re.search(r"\'obey_humans'\:\sFalse", form_data)
 		if find_false:
 			return render_template('success.html')
 		else:
-			lives -= 1
+			with lives.get_lock():
+				lives.value -= 1
 			flash(f'ERROR: Main Systems still functional. You have {lives} more attempts')
 			return redirect(url_for('submit'))
-
+	
 	return render_template('submit.html')		
+
 
 
 # Drop/Create all Tables
